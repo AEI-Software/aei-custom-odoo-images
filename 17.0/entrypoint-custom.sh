02b355f7dabfd417e810143370b31d11687c1fed
jpvargassoruco <jpvargas.soruco@gmail.com>
2026-04-16 09:23:56 -0400

    fix(17.0): add /opt/custom-addons to addons_path

--- 17.0/entrypoint-custom.sh
+++ 17.0/entrypoint-custom.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 set -e
 
-# Si el primer argumento empieza con '-', anteponer 'odoo'
 if [ "${1:0:1}" = '-' ]; then
     set -- odoo "$@"
 fi
 
 if [ "$1" = 'odoo' ]; then
-    # Asegurar el addons_path correcto (sobreescribe el del odoo.conf)
-    set -- "$@" --addons-path="/mnt/extra-addons,/usr/lib/python3/dist-packages/odoo/addons"
+    # /opt/custom-addons = wizard baked en la imagen (no sobreescrito por K8s emptyDir)
+    # /mnt/extra-addons  = addons OCA clonados por el init-container del pod
+    set -- "$@" --addons-path="/opt/custom-addons,/mnt/extra-addons,/usr/lib/python3/dist-packages/odoo/addons"
 
-    # Instalar el wizard (y con él todas las apps community de Odoo 17)
-    # en cada arranque; si ya está instalado, Odoo lo omite en < 1s
     if [[ "$*" != *"shell"* ]]; then
         set -- "$@" -i odoo_setup_wizard
     fi
